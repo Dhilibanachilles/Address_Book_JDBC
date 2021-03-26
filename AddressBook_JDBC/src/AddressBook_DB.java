56a3cc664b96b5506682ae4b13f6cba8a151b06e
dhiliban <dhiliban.baskar@gmail.com>
2021-03-26 10:15:56 +0530

    Adding multiple contacts to addressbook

---
+++ AddressBook_JDBC/src/AddressBook_DB.java
@@ -0,0 +1,109 @@
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class AddressBook_DB {
+
+	 private AddressBook_DataBaseService addressBookService;
+	List<AddressBookData> addressBookList = new ArrayList<>();
+	
+	public AddressBook_DB() {
+		addressBookService = AddressBook_DataBaseService.getInstance();
+	}
+
+	public AddressBook_DB(List<AddressBookData> addressBookList) {
+		this();
+		this.addressBookList = addressBookList;
+	}
+	
+	public List<AddressBookData> readData() {
+		this.addressBookList = addressBookService.readData(); 
+		return  addressBookList;
+	}
+
+	public void updateAddressBookData(String firstname, String city)
+	{
+		int result = new AddressBook_DataBaseService().updateEmployeeSalaryResult(firstname, city);
+		if(result == 0) return;
+		AddressBookData AddressBookData = this.getAddressBookData(firstname);
+		if(AddressBookData != null) AddressBookData.setCity(city);
+	}
+	
+	private AddressBookData getAddressBookData(String firstname) {
+      AddressBookData addressBookData ;
+      addressBookData = this.addressBookList.stream()
+    		             .filter(addressBookEntry  ->  (addressBookEntry.getFirstname()).equals(firstname))
+    		             .findFirst()
+    		             .orElse(null);
+		return addressBookData;
+	}
+
+	public boolean checkAddressBookDataSyncWithDB(String firstname) {
+		try {
+			return addressBookService.getAddressBookData(firstname).get(0).getFirstname().equals(getAddressBookData(firstname).getFirstname());
+		} catch (IndexOutOfBoundsException e) {
+		}
+		return false;
+	}
+
+	public List<AddressBookData> getAddressBookDataForDateRange(Date startDate, Date endDate) {
+		return addressBookService.getAddressBookDataForDateRange( startDate,  endDate);
+	}
+
+	public List<AddressBookData> getAddressBookDataByCity(String city) {
+		return addressBookService.getAddressBookDataByCity(city);
+	}
+
+	public List<AddressBookData> getAddressBookDataByState(String state) {
+		return addressBookService.getAddressBookDataByState(state);
+	}
+
+	public void addContactToAddressBook(String firstname, String lastname, String address, String city, String state,
+			int zip, int phonenumber, String email, Date date) {
+           addressBookList.add(addressBookService.addContactToAddressBook(firstname,lastname,address,city,state,zip,phonenumber,email,date));		
+	}
+
+	int count= 0;
+	public int addContactToAddressBook(List<AddressBookData> asList) {
+		Map<Integer,Boolean> contactAdditionStatus = new HashMap<Integer, Boolean>();
+		asList.forEach(addressBookData ->{
+			Runnable task = () -> {
+				contactAdditionStatus.put(addressBookData.hashCode(),false);
+				System.out.println("Employee Being Added: "+Thread.currentThread().getName());
+				this.addContactToAddressBook(addressBookData.getFirstname(),
+						addressBookData.getLastname(), addressBookData.getAddress()
+				        , addressBookData.getCity(),addressBookData.getState(),addressBookData.getZip(),addressBookData.getPhonenumber()
+				        , addressBookData.getEmail(),addressBookData.getDate());
+				contactAdditionStatus.put(addressBookData.hashCode(), true);
+				System.out.println("Employee Added : "+Thread.currentThread().getName());
+				count++;
+			};
+			Thread thread = new Thread(task,addressBookData.getFirstname());
+			thread.start();
+
+		});
+		while(contactAdditionStatus.containsValue(false)) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(count);
+		System.out.println(this.addressBookList);	
+		return count;
+	
+	}
+
+	public long countEntries() {
+	 return addressBookList.size();
+	}
+}
---
+++ AddressBook_JDBC/src/Testing_AddressBook.java
@@ -0,0 +1,92 @@
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class Testing_AddressBook
+{
+    AddressBook_DB addressBook = new AddressBook_DB() ;
+    
+   
+         @Test
+        public void  givenAddressBookData_WhenReturned_ShouldMatchEntryCount()
+        {
+        	List<AddressBookData> addressBookList = addressBook.readData();
+        	Assert.assertEquals(7,addressBookList.size());
+        }
+         
+    
+ 		@Test
+ 		public void givenCity_WhenUpdated_ShouldSyncWithDatabaseUsingPreparedStatement()
+ 		{
+ 			List<AddressBookData> addressBookData = addressBook.readData();
+ 			addressBook.updateAddressBookData("Priya","Chandigarh");
+ 			boolean result = addressBook.checkAddressBookDataSyncWithDB("Priya");
+ 			Assert.assertTrue(result);
+ 		}
+ 		
+ 		
+ 				@Test 
+ 				public void givenDateRange_WhenRetrieved_ShouldMatchContactEntriesCount()
+ 				{
+ 					List<AddressBookData> addressBookData = addressBook.readData();
+ 		            LocalDate startDate = LocalDate.of(2018,01,01);
+ 		            LocalDate endDate = LocalDate.now();
+ 		    		addressBookData = addressBook.getAddressBookDataForDateRange(Date.valueOf(startDate), Date.valueOf(endDate));
+ 		    		Assert.assertEquals(7, addressBookData.size());
+ 				}
+ 				
+ 				
+ 				@Test 
+ 				public void givenCity_WhenRetrieved_ShouldMatchContactEntriesCount()
+ 				{
+ 					List<AddressBookData> addressBookData = addressBook.readData();
+ 		            String city = "gurgaon";
+ 		    		addressBookData = addressBook.getAddressBookDataByCity(city);
+ 		    		Assert.assertEquals(3, addressBookData.size());
+ 				}
+ 				
+ 				@Test 
+ 				public void givenState_WhenRetrieved_ShouldMatchContactEntriesCount()
+ 				{
+ 					List<AddressBookData> addressBookData = addressBook.readData();
+ 		            String state = "haryana";
+ 		    		addressBookData = addressBook.getAddressBookDataByState(state);
+ 		    		Assert.assertEquals(7, addressBookData.size());
+ 				}
+
+ 			
+ 				@Test
+ 				public void givenNewContact_WhenAdded_ShouldSyncWithDatabase() {
+ 					addressBook.readData();
+ 					addressBook.addContactToAddressBook("balbir", "gahlawat","khanpur","jhajjar","haryana",123455,12324435,"balbir@abc.com",Date.valueOf(LocalDate.of(2019,12,23)));
+ 					boolean result = addressBook.checkAddressBookDataSyncWithDB("balbir");
+ 					Assert.assertTrue(result);
+ 				}
+
+ 			
+ 				@Test
+ 				public void given4Contacts_WhenAddedUsingThreads_ShouldMatchEmployeeEntries()
+ 				{
+ 					AddressBookData[] arrayOfContacts = {
+ 							new AddressBookData("sharwan", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23))),
+ 							new AddressBookData("ankur", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23))),
+ 							new AddressBookData("Bittu", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23))),
+ 							new AddressBookData("Amit", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23)))
+ 							 					};
+ 					addressBook.readData();
+ 					Instant start = Instant.now();
+ 					int count = addressBook.addContactToAddressBook(Arrays.asList(arrayOfContacts));
+ 					Instant end = Instant.now();
+ 					System.out.println("Duration With Thread: "+java.time.Duration.between(start, end));
+ 					Assert.assertEquals(12, count+8);
+ 				}
+ 			}
