c9d7cb31b359e4ea78c2135a5f0be07ffc747e39
dhiliban <dhiliban.baskar@gmail.com>
2021-03-27 10:22:41 +0530

    AddingNewContacts

--- AddressBook_JDBC/src/Testing_AddressBook.java
+++ AddressBook_JDBC/src/Testing_AddressBook.java
@@ -1,22 +1,18 @@
-
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Date;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class Testing_AddressBook
 {
     AddressBook_DB addressBook = new AddressBook_DB() ;
     
-  
+
          @Test
         public void  givenAddressBookData_WhenReturned_ShouldMatchEntryCount()
         {
@@ -24,7 +20,7 @@ public class Testing_AddressBook
         	Assert.assertEquals(7,addressBookList.size());
         }
          
-    
+     
  		@Test
  		public void givenCity_WhenUpdated_ShouldSyncWithDatabaseUsingPreparedStatement()
  		{
@@ -34,7 +30,7 @@ public class Testing_AddressBook
  			Assert.assertTrue(result);
  		}
  		
- 	
+ 		
  				@Test 
  				public void givenDateRange_WhenRetrieved_ShouldMatchContactEntriesCount()
  				{
@@ -45,7 +41,7 @@ public class Testing_AddressBook
  		    		Assert.assertEquals(7, addressBookData.size());
  				}
  				
- 			
+ 				
  				@Test 
  				public void givenCity_WhenRetrieved_ShouldMatchContactEntriesCount()
  				{
@@ -61,10 +57,10 @@ public class Testing_AddressBook
  					List<AddressBookData> addressBookData = addressBook.readData();
  		            String state = "haryana";
  		    		addressBookData = addressBook.getAddressBookDataByState(state);
- 		    		Assert.assertEquals(7, addressBookData.size());
+ 		    		Assert.assertEquals(6, addressBookData.size());
  				}
 
- 		
+ 			
  				@Test
  				public void givenNewContact_WhenAdded_ShouldSyncWithDatabase() {
  					addressBook.readData();
@@ -72,22 +68,4 @@ public class Testing_AddressBook
  					boolean result = addressBook.checkAddressBookDataSyncWithDB("balbir");
  					Assert.assertTrue(result);
  				}
-
- 			
- 				@Test
- 				public void given4Contacts_WhenAddedUsingThreads_ShouldMatchEmployeeEntries()
- 				{
- 					AddressBookData[] arrayOfContacts = {
- 							new AddressBookData("sharwan", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23))),
- 							new AddressBookData("ankur", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23))),
- 							new AddressBookData("Bittu", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23))),
- 							new AddressBookData("Amit", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23)))
- 							 					};
- 					addressBook.readData();
- 					Instant start = Instant.now();
- 					int count = addressBook.addContactToAddressBook(Arrays.asList(arrayOfContacts));
- 					Instant end = Instant.now();
- 					System.out.println("Duration With Thread: "+java.time.Duration.between(start, end));
- 					Assert.assertEquals(12, count+8);
- 				}
- 			}
+}
