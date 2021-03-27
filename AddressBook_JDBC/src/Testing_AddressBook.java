e4bac9691c0d08d9cae5ac4d8445a67484243997
dhiliban <dhiliban.baskar@gmail.com>
2021-03-27 10:29:20 +0530

    To delete Contact Entry From JSON

--- AddressBook_JDBC/src/Testing_AddressBook.java
+++ AddressBook_JDBC/src/Testing_AddressBook.java
@@ -1,8 +1,10 @@
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Date;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
@@ -12,20 +14,20 @@ public class Testing_AddressBook
 {
     AddressBook_DB addressBook = new AddressBook_DB() ;
     
-
+    
          @Test
         public void  givenAddressBookData_WhenReturned_ShouldMatchEntryCount()
         {
         	List<AddressBookData> addressBookList = addressBook.readData();
-        	Assert.assertEquals(7,addressBookList.size());
+        	Assert.assertEquals(12,addressBookList.size());
         }
          
      
  		@Test
  		public void givenCity_WhenUpdated_ShouldSyncWithDatabaseUsingPreparedStatement()
  		{
- 			List<AddressBookData> addressBookData = addressBook.readData();
- 			addressBook.updateAddressBookData("Priya","Chandigarh");
+ 			addressBook.readData();
+ 			addressBook.updateAddressBookData("Priya","chandigarh");
  			boolean result = addressBook.checkAddressBookDataSyncWithDB("Priya");
  			Assert.assertTrue(result);
  		}
@@ -35,10 +37,10 @@ public class Testing_AddressBook
  				public void givenDateRange_WhenRetrieved_ShouldMatchContactEntriesCount()
  				{
  					List<AddressBookData> addressBookData = addressBook.readData();
- 		            LocalDate startDate = LocalDate.of(2018,01,01);
+ 		            LocalDate startDate = LocalDate.of(2019,01,01);
  		            LocalDate endDate = LocalDate.now();
  		    		addressBookData = addressBook.getAddressBookDataForDateRange(Date.valueOf(startDate), Date.valueOf(endDate));
- 		    		Assert.assertEquals(7, addressBookData.size());
+ 		    		Assert.assertEquals(8, addressBookData.size());
  				}
  				
  				
@@ -46,9 +48,9 @@ public class Testing_AddressBook
  				public void givenCity_WhenRetrieved_ShouldMatchContactEntriesCount()
  				{
  					List<AddressBookData> addressBookData = addressBook.readData();
- 		            String city = "gurgaon";
+ 		            String city = "jaipur";
  		    		addressBookData = addressBook.getAddressBookDataByCity(city);
- 		    		Assert.assertEquals(3, addressBookData.size());
+ 		    		Assert.assertEquals(1, addressBookData.size());
  				}
  				
  				@Test 
@@ -57,7 +59,7 @@ public class Testing_AddressBook
  					List<AddressBookData> addressBookData = addressBook.readData();
  		            String state = "haryana";
  		    		addressBookData = addressBook.getAddressBookDataByState(state);
- 		    		Assert.assertEquals(6, addressBookData.size());
+ 		    		Assert.assertEquals(7, addressBookData.size());
  				}
 
  			
@@ -68,4 +70,22 @@ public class Testing_AddressBook
  					boolean result = addressBook.checkAddressBookDataSyncWithDB("balbir");
  					Assert.assertTrue(result);
  				}
-}
+
+ 			
+ 				@Test
+ 				public void given4Contacts_WhenAddedUsingThreads_ShouldMatchEmployeeEntries()
+ 				{
+ 					AddressBookData[] arrayOfContacts = {
+ 							new AddressBookData("sharwan", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23))),
+ 							new AddressBookData("ankur", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23))),
+ 							new AddressBookData("Bittu", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23))),
+ 							new AddressBookData("Amit", "singh","khanpur","south","delhi",123455,12324435,"sharwan@abc.com",Date.valueOf(LocalDate.of(2019,12,23)))
+ 							 					};
+ 					addressBook.readData();
+ 					Instant start = Instant.now();
+ 					int count = addressBook.addContactToAddressBook(Arrays.asList(arrayOfContacts));
+ 					Instant end = Instant.now();
+ 					System.out.println("Duration With Thread: "+java.time.Duration.between(start, end));
+ 					Assert.assertEquals(12, count+8);
+ 				}
+ 			}
--- AddressBook_JDBC/src/Testing_AddressBookJSONServer.java
+++ AddressBook_JDBC/src/Testing_AddressBookJSONServer.java
@@ -1,13 +1,16 @@
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Arrays;
 
+import org.apache.http.impl.conn.SystemDefaultDnsResolver;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import com.google.gson.Gson;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class Testing_AddressBookJSONServer {
 
@@ -16,23 +19,123 @@ public class Testing_AddressBookJSONServer {
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = 3000;
 	}
-	
-	private AddressBookData[] getEmployeeList() {
-		Response response = RestAssured.get("/Contacts");
-		System.out.println("Address Book Contacts  in json server  are : \n"+response.asString());
-		AddressBookData[] arrayOfContacts = new Gson().fromJson(response.asString(),AddressBookData[].class);
+
+	private AddressBookData[] getContactList() {
+		Response response = RestAssured.get("/AddressBook");
+		System.out.println("Address Book Contacts  in json server  are : \n" + response.asString());
+		AddressBookData[] arrayOfContacts = new Gson().fromJson(response.asString(), AddressBookData[].class);
 		return arrayOfContacts;
+	}
+
+	private Response addContactToJsonServer(AddressBookData addressBookData) {
+		String contact = new Gson().toJson(addressBookData);
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+		request.body(contact);
+		//System.out.println(contact);
+		return request.post("/AddressBook");
 	}
 
 	
 	@Test
-	public void givenAddressBookDataInJSONServer_WhenRetreived_ShouldMatchCount()
-	{
-		AddressBookData[] arrayOfContacts = getEmployeeList();
+	public void givenAddressBookDataInJSONServer_WhenRetreived_ShouldMatchCount() {
+		AddressBookData[] arrayOfContacts = getContactList();
 		AddressBook_DB contact;
 		contact = new AddressBook_DB(Arrays.asList(arrayOfContacts));
 		long entries = contact.countEntries();
-		Assert.assertEquals(2,entries);
+		Assert.assertEquals(2, entries);
 	}
 	
+	
+		@Test
+		public void givenNewEmployee_WhenAdded_ShouldMatchCountand201ResponseAndCount() throws SQLException
+		{
+			AddressBookData[] arrayOfEmps = getContactList();
+		     AddressBook_DB addressBook;
+			addressBook = new AddressBook_DB(Arrays.asList(arrayOfEmps));
+
+			AddressBookData addressBookData = null;
+			addressBookData = new AddressBookData(0,"sharwan", "singh", "khanpur", "south", "delhi", 123455, 12324435,
+					"sharwan@abc.com");
+			Response response = addContactToJsonServer(addressBookData);
+			int statusCode = response.getStatusCode();
+			System.out.println(statusCode);
+			Assert.assertEquals(201,statusCode);
+
+			addressBookData = new Gson().fromJson(response.asString(),AddressBookData.class);
+			addressBook.addContactToBook(addressBookData);
+			long entries = addressBook.countEntries();
+			Assert.assertEquals(3,entries);
+		}
+
+	
+	@Test
+	public void givenMultipleContacts_WhenAdded_ShouldMatchCountand201ResponseAndCount() throws SQLException {
+		AddressBookData[] arrayOfContacts = getContactList();
+		AddressBook_DB addressBookDB;
+		addressBookDB = new AddressBook_DB(Arrays.asList(arrayOfContacts));
+
+		AddressBookData[] arrayOfContactEntries = {
+				new AddressBookData(0,"sharwan", "singh", "khanpur", "south", "delhi", 123455, 12324435,
+						"sharwan@abc.com"),
+				new AddressBookData(0,"ankur", "singh", "khanpur", "south", "delhi", 123455, 12324435, "ankur@abc.com"),
+				new AddressBookData(0,"Bittu", "singh", "khanpur", "south", "delhi", 123455, 12324435, "bittu@abc.com"),
+				new AddressBookData(0,"Amit", "singh", "khanpur", "south", "delhi", 123455, 12324435, "amit@abc.com") 
+				};
+
+		for (AddressBookData addressBookData : arrayOfContactEntries) {
+			Response response = addContactToJsonServer(addressBookData);
+			int statusCode = response.getStatusCode();
+			Assert.assertEquals(201, statusCode);
+
+			addressBookData = new Gson().fromJson(response.asString(), AddressBookData.class);
+			addressBookDB.addContactToBook(addressBookData);
+		}
+		long entries = addressBookDB.countEntries();
+		Assert.assertEquals(7, entries);
+	}
+	
+	
+	  @Test 
+	  public void givenNewCityToContact_WhenUpdated_ShouldMatch200Response()
+	  {
+		  AddressBookData[] arrayOfContacts = getContactList();
+			AddressBook_DB addressBookDB;
+			addressBookDB = new AddressBook_DB(Arrays.asList(arrayOfContacts));
+
+			addressBookDB.updateAddressBookDataJSONServer("Amit", "jaipur");
+			AddressBookData addressBookData = addressBookDB.getAddressBookData("Amit");
+
+			 String empJson = new Gson().toJson(addressBookData);
+	          RequestSpecification request = RestAssured.given();
+	          request.header("Content-Type","application/json");
+	          request.body(empJson);
+              System.out.println(addressBookData.getId());
+              System.out.println(addressBookData.getCity());
+	          Response response = request.put("/AddressBook/"+addressBookData.getId());
+				int statusCode = response.getStatusCode();
+				Assert.assertEquals(200,statusCode);
+	  }
+	  
+	  @Test 
+	  public void givenContactToDelete_WhenDeleted_ShouldMatch200Response()
+	  {
+		  AddressBookData[] arrayOfContacts = getContactList();
+			AddressBook_DB addressBookDB;
+			addressBookDB = new AddressBook_DB(Arrays.asList(arrayOfContacts));
+
+			AddressBookData addressBookData = addressBookDB.getAddressBookData("sharwan");
+
+	          RequestSpecification request = RestAssured.given();
+	          request.header("Content-Type","application/json");
+	          Response response = request.delete("/AddressBook/"+addressBookData.getId());
+				int statusCode = response.getStatusCode();
+				Assert.assertEquals(200,statusCode);
+
+				addressBookDB.deleteContactFromJSON(addressBookData.getFirstname());
+				long entries = addressBookDB.countEntries();
+				Assert.assertEquals(6,entries);
+	  }
+
+
 }
